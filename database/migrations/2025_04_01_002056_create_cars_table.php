5721f7fef6dc61ce9e150eebc55344682f4ab423
Rajarshe <rajarshe@umich.edu>
2025-04-02 11:51:50 -0400

    car table update

--- database/migrations/2025_04_01_002056_create_cars_table.php
+++ database/migrations/2025_04_01_002056_create_cars_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
     {
         Schema::create('cars', function (Blueprint $table) {
             $table->id();
+            $table->string('model');
+            $table->integer('year');
+            $table->integer('mileage');
+            $table->string('availability_calendar'); 
+            $table->string('pick_up_location');
+            $table->decimal('rental_pricing', 10, 2);
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
