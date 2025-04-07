01f3458375d7083e0f9ecd747a9aca73de837cf5
ShahimaRa <shahimatasnim12345@gmail.com>
2025-04-07 13:15:49 -0400

    user controller class create and route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CarController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +23,12 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/user-login', [UserController::class, 'index'])->name('user.login');
+Route::post('/user-login', [UserController::class, 'login'])->name('user.login.submit');
+Route::post('/user-logout', [UserController::class, 'logout'])->name('user.logout');
+Route::get('/user-register', [UserController::class, 'create'])->name('user.register');
+Route::post('/user-register-create', [UserController::class, 'store'])->name('user.register.store');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
