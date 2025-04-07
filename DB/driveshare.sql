efdb8d6b1d0e50fe0251d64406ed33f55f735e60
ShahimaRa <shahimatasnim12345@gmail.com>
2025-04-07 18:25:02 -0400

    db update

--- DB/driveshare.sql
+++ DB/driveshare.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 03, 2025 at 12:49 AM
+-- Generation Time: Apr 08, 2025 at 12:22 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -37,16 +37,19 @@ CREATE TABLE `cars` (
   `rental_pricing` decimal(10,2) NOT NULL,
   `image` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `cars`
 --
 
-INSERT INTO `cars` (`id`, `model`, `year`, `mileage`, `availability_calendar`, `pick_up_location`, `rental_pricing`, `image`, `created_at`, `updated_at`) VALUES
-(1, 'Accord SE', 2015, 102000, '\"23\\/04\\/2025 - 23\\/05\\/2025\"', 'Location 20289', 100.00, 'cars/ytadbtJTn8fXjSujRfF11gtoQI8j5s38rRamxvDo.jpg', '2025-04-02 20:15:05', '2025-04-02 20:15:05'),
-(3, 'Toyota AC', 2019, 47000, '\"22\\/04\\/2025 - 25\\/05\\/2025\"', 'Michgan', 120.00, NULL, '2025-04-02 20:51:51', '2025-04-02 20:51:51');
+INSERT INTO `cars` (`id`, `model`, `year`, `mileage`, `availability_calendar`, `pick_up_location`, `rental_pricing`, `image`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'Accord SE', 2015, 102000, '\"23\\/04\\/2025 - 23\\/05\\/2025\"', 'Location 20289', 100.00, 'cars/ytadbtJTn8fXjSujRfF11gtoQI8j5s38rRamxvDo.jpg', '2025-04-02 20:15:05', '2025-04-02 20:15:05', NULL),
+(2, 'Toyota AC', 2019, 47000, '\"22\\/04\\/2025 - 25\\/05\\/2025\"', 'Michgan', 120.00, NULL, '2025-04-02 20:51:51', '2025-04-02 20:51:51', NULL),
+(3, 'Accord ', 2015, 102000, '\"23\\/04\\/2025 - 23\\/05\\/2025\"', 'Location 20289', 100.00, 'cars/ytadbtJTn8fXjSujRfF11gtoQI8j5s38rRamxvDo.jpg', '2025-04-03 00:15:05', '2025-04-03 00:15:05', NULL),
+(4, 'Toyota', 2019, 47000, '\"22\\/04\\/2025 - 25\\/05\\/2025\"', 'Michgan', 120.00, NULL, '2025-04-03 00:51:51', '2025-04-03 00:51:51', NULL);
 
 -- --------------------------------------------------------
 
@@ -86,7 +89,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
 (5, '2025_04_01_002056_create_carsgryjus_table', 2),
-(6, '2025_04_01_002056_create_cars_table', 3);
+(6, '2025_04_01_002056_create_cars_table', 3),
+(7, '2025_04_02_172433_add_deleted_at_to_cars_table', 4),
+(8, '2025_04_07_213508_add_security_questions_to_users_table', 5);
 
 -- --------------------------------------------------------
 
@@ -127,8 +132,11 @@ CREATE TABLE `personal_access_tokens` (
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
+  `security_answer_1` varchar(250) DEFAULT NULL,
+  `security_answer_2` varchar(250) DEFAULT NULL,
+  `security_answer_3` varchar(250) DEFAULT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
   `password` varchar(255) NOT NULL,
   `remember_token` varchar(100) DEFAULT NULL,
@@ -140,8 +148,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'Farhan', 'farhan@g.c', NULL, '$2y$10$PKd6YGZdF2QoWO0ypLp/ReP8ePDI.IbHLxym2HvERBsC6.ctvIab.', NULL, '2025-04-01 01:53:19', '2025-04-01 01:53:19');
+INSERT INTO `users` (`id`, `name`, `email`, `security_answer_1`, `security_answer_2`, `security_answer_3`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Farhan', 'farhan@g.c', NULL, NULL, NULL, NULL, '$2y$10$PKd6YGZdF2QoWO0ypLp/ReP8ePDI.IbHLxym2HvERBsC6.ctvIab.', NULL, '2025-04-01 01:53:19', '2025-04-01 01:53:19'),
+(2, NULL, 'farhan2@g.c', 'a', 'a', 'a', NULL, '$2y$10$UXWUBq6bVzJCI5.muWgPtepTi08kgnOI.m9qJ4AFLCiHo9JTwR2jC', NULL, '2025-04-08 01:54:27', '2025-04-08 01:54:27');
 
 --
 -- Indexes for dumped tables
@@ -195,7 +204,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -207,7 +216,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `personal_access_tokens`
@@ -219,7 +228,7 @@ ALTER TABLE `personal_access_tokens`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
