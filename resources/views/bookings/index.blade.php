87139670d16cf1589179a737cd17fe55a89046ad
MieshaRahman <rmiesha@umich.edu>
2025-04-09 13:36:16 -0400

    add notification option

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -10,6 +10,7 @@
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 <body>
@@ -36,7 +37,7 @@
                     <tr>
                         <td>{{ $booking->id }}</td>
                         <td>
-                            {{ $booking->user->name }} <br> 
+                            {{ $booking->user->name }} <br>
                             {{ $booking->user->email }}
 
                         </td>
--- resources/views/cars/edit.blade.php
+++ resources/views/cars/edit.blade.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 <body>
@@ -47,7 +48,7 @@
                 <input type="number" step="0.01" name="rental_pricing" class="form-control" id="rental_pricing"
                     value="{{ $car->rental_pricing }}" required>
             </div>
-            
+
             <button type="submit" class="btn btn-primary">Save Changes</button>
         </form>
     </div>
